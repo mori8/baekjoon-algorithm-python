6b60255b657438e3f0eca6b8b0d2c9e34d153417
mori8 <kaithape@gmail.com>
2020-07-13 22:37:14 +0900

    solved 11053, 11055(dp)

---
+++ 10000+/bj_11053.py
@@ -0,0 +1,12 @@
+n = int(input())
+dp = [0 for _ in range(n)] # i번째 수를 마지막으로 하는 IS 중 가장 긴 것의 길이. dp[]를 어떻게 채울 것인가?
+arr = list(map(int, input().split()))
+
+for i in range(n):
+    maximum = 0
+    for j in range(i):
+        if maximum < dp[j] and arr[j] < arr[i]:
+            maximum = dp[j]
+    dp[i] = maximum + 1
+
+print(max(dp))
