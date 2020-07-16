702bbf5e1aefe1753efed719284f4bd190b898a9
mori8 <kaithape@gmail.com>
2020-07-16 15:02:59 +0900

    solved 2749, 11049

--- 2000/bj_2749.py
+++ 2000/bj_2749.py
@@ -1,9 +1,10 @@
+# 피보나치 수 2 -> 피사노 주기
 n = int(input())
 
 fibo = [0, 1]
 
-for i in range(2, 1000000000000000001):
+for i in range(2, 1500001):
     num = fibo[i-1] + fibo[i-2]
     fibo.append(num % 1000000)
 
-print(fibo[n])
+print(fibo[n % 1500000])
